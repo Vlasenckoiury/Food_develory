7e075f47e33aa3439f931065b1f6a3d8417f6801
vlase <vlasencko_iury1999@yandex.by>
2023-11-24 00:33:05 +0300

    модели

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,18 @@
+## -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Pizza(models.Model):
+    pizza_name = models.CharField(verbose_name='Название пиццы', max_length=20)
+    pizza_img = models.TextField(verbose_name='Картинка')
+    pizza_price = models.CharField(verbose_name='Цена', max_length=10)
+    pizza_weight = models.TextField(verbose_name='Вес')
+    pizza_description = models.TextField(verbose_name='Состав')
+
+    def __str__(self):
+        return self.pizza_name
+
+    class Meta:
+        verbose_name = 'Пицца'
+        verbose_name_plural = 'Пиццы'
+        ordering = ['-id']  # сортировка(фильтр)
